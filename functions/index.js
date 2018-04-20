adc97a49d3b8e87ae9a4754587196f0a0bc45dba
EvanMasterson <evanm95@hotmail.com>
2018-04-20 16:26:41 +0100

    update version, function to check when watch lat/lng values changes

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.checkLatLng = functions.database.ref('{userId}/watch').onUpdate((snapshot, context) => {
+    let latitude;
+    let longitude;
+    const userId = context.params.userId.toString();
+
+    snapshot.after.forEach(data =>{
+        let key = data.key.toString();
+        let value = data.val().toString();
+
+        if(key==='latitude'){
+            latitude = parseFloat(value);
+            console.log("Latitude: "+latitude);
+        }
+        if(key==='longitude'){
+            longitude = parseFloat(value);
+            console.log("Longitude: " + longitude);
+        }
+    });
+    return true;
+});
