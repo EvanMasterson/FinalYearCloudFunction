0f70da23e1e2de9940ba09c0a21dfdb16b59df11
EvanMasterson <evanm95@hotmail.com>
2018-04-22 20:48:26 +0100

    read user tokenId, read and create array of points in each zone

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,10 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.checkLatLng = functions.database.ref('{userId}/watch').onUpdate((snapshot, context) => {
+exports.checkLatLng = functions.database.ref('{userId}').onUpdate((snapshot, context) => {
     let latitude;
     let longitude;
+    let tokenId;
     const userId = context.params.userId.toString();
 
     snapshot.after.forEach(data =>{
@@ -12,13 +13,41 @@ exports.checkLatLng = functions.database.ref('{userId}/watch').onUpdate((snapsho
         let value = data.val().toString();
 
         if(key==='latitude'){
-            latitude = parseFloat(value);
+            latitude = value;
             console.log("Latitude: "+latitude);
         }
         if(key==='longitude'){
-            longitude = parseFloat(value);
+            longitude = value;
             console.log("Longitude: " + longitude);
+        }
+        if(key==='tokenId'){
+            tokenId = value;
+            console.log("TokenId: " + tokenId);
+        }
+        if(key==='zones'){
+            data.forEach(zoneData =>{
+                let zone=[];
+                console.log("ZoneKey: "+zoneData.key.toString());
+                for(let i=0; i<zoneData.val().length; i++){
+                    let lat = zoneData.val()[i].latitude;
+                    let lng = zoneData.val()[i].longitude ;
+                    zone.push({"latitude": lat, "longitude": lng})
+                }
+                if(latitude && longitude && zone){
+                   checkLocation(latitude, longitude, zone);
+                   zone=[];
+                }
+            });
         }
     });
     return true;
 });
+
+function checkLocation(latitude, longitude, zone){
+    // TODO check if lat lng is inside the zone
+    console.log(latitude, longitude, zone);
+}
+
+function sendNotification(){
+    // TODO send notification using tokenId to users device
+}
