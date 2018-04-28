2595931816daba9a99d438f1361f6a393b7ca85c
EvanMasterson <evanm95@hotmail.com>
2018-04-28 20:06:49 +0100

    twilio api, send automated texts to user if phone number exists in their profile

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,30 @@
+// Require firebase functions/auth
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 // npm package to check if latlng point is inside polygon
 const checkInside = require('point-in-polygon');
+/*
+    Require Twilio service
+    Firebase Flame or Blaze plan required to make calls to external api's
+ */
+const twilio = require('twilio');
+// Set these credentials using cmd line with: firebase functions:config:set twilio.sid="CHANGE_ME" twilio.token="CHANGE_ME"
+const accountSid = functions.config().twilio.sid;
+const authToken = functions.config().twilio.token;
+
+// Initialise app
 admin.initializeApp();
 
+/*
+    Cloud function to listen for changes in users values
+    On changing, triggers function to check if user location has entered zone or not
+    Given sufficient tokenId and phone number, push notifications and twilio automated texts triggered
+ */
 exports.checkLatLng = functions.database.ref('{userId}').onUpdate((snapshot, context) => {
     let latitude;
     let longitude;
     let tokenId;
+    let phone;
     const userId = context.params.userId.toString();
 
     snapshot.after.forEach(data =>{
@@ -26,6 +43,10 @@ exports.checkLatLng = functions.database.ref('{userId}').onUpdate((snapshot, con
             tokenId = value;
             console.log("TokenId: " + tokenId);
         }
+        if(key==='phone'){
+            phone = value;
+            console.log("Phone: " + phone)
+        }
         if(key==='zones'){
             data.forEach(zoneData =>{
                 let zone=[];
@@ -35,9 +56,12 @@ exports.checkLatLng = functions.database.ref('{userId}').onUpdate((snapshot, con
                     let lng = zoneData.val()[i].longitude ;
                     zone.push([lat, lng])
                 }
-                if(latitude && longitude && zone && tokenId){
-                   checkLocation(latitude, longitude, zone, tokenId);
+                if(latitude && longitude && zone && tokenId && phone){
+                   checkLocation(latitude, longitude, zone, tokenId, phone);
                    zone=[];
+                } else if(latitude && longitude && zone && tokenId){
+                    checkLocation(latitude, longitude, zone, tokenId);
+                    zone=[];
                 }
             });
         }
@@ -46,14 +70,18 @@ exports.checkLatLng = functions.database.ref('{userId}').onUpdate((snapshot, con
 });
 
 /*
-Function to check if current user location is inside a zone or not
-If it returns true, we send a notification to that user using their device tokenId
+    Function to check if current user location is inside a zone or not
+    If it returns true, we send a notification to that user using their device tokenId
+    If user has registered a phone number we also send a text message using twilio api
  */
-function checkLocation(latitude, longitude, zone, tokenId){
+function checkLocation(latitude, longitude, zone, tokenId, phone){
     console.log(latitude, longitude, zone);
     if(checkInside([latitude, longitude], zone)) {
         console.log("Inside zone");
         sendNotification(tokenId);
+        if(phone){
+            sendTextMessage(phone);
+        }
     } else {
         console.log("Not in zone");
     }
@@ -65,7 +93,7 @@ function sendNotification(tokenId){
         token: tokenId,
         notification: {
             title: "Geofence Monitor",
-            body: "ALERT !!!\nUser has entered an unsafe area\nOpen to view current status"
+            body: "ALERT !!!\nUser has entered an unsafe area.\nOpen to view current status."
         },
         android: {
             notification: {
@@ -77,5 +105,20 @@ function sendNotification(tokenId){
         console.log("Success sending: " + response);
     }).catch((error) =>{
         console.log("Error sending message: " + error);
+    });
+}
+
+// Function triggered by checkLocation to send automated text to user with twilio api
+function sendTextMessage(phone){
+    console.log(phone);
+    const client = new twilio(accountSid, authToken);
+    client.messages.create({
+        body: "Automated text ALERT !!!\nUser has entered an unsafe area.\nOpen application for more info.",
+        to: phone,
+        from: '+16308668643',
+    }).then((message) => {
+        console.log(message.sid);
+    }).catch((error) => {
+        console.log(error);
     });
 }
